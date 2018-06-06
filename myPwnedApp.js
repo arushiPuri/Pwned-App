815d4bf7afcc0c21b593915a00347f77134f86c5
Arushi Puri <arushipuri786@gmail.com>
2018-06-06 22:14:36 +1000

    Add error message if not pwned

--- myPwnedApp.js
+++ myPwnedApp.js
@@ -7,6 +7,9 @@ app.controller('ctrl', function($scope, $http){
   $scope.includeUnverified = false;
 //crossCheck is a function
   $scope.crossCheck = function(){
+    //This is to clear screen when the function is called.
+    $scope.breachedData = '';
+    $scope.error = '';
     //variable url is assigned to the account and additionally the email entered by user.
     //If the value of includeUnverified is true, includeUnverified is also added.
     var url = 'https://haveibeenpwned.com/api/v2/breachedaccount/' + $scope.email + ($scope.includeUnverified ? '?includeUnverified=true' : '');
@@ -18,6 +21,9 @@ app.controller('ctrl', function($scope, $http){
         $scope.breachedData = response.data;
       },
       function(response){
+        if(response.status == 404){
+          $scope.error = "Congrats! You have not been Pwned!! :D"
+        }
         console.log("Error " + response.status);
       }
     );
